97ebed9176ad559024ff8b320a13e645a762585d
Ben Armston <ben.armston@alces-software.com>
2022-06-16 16:02:15 +0100

    Bump version

--- lib/service/version.rb
+++ lib/service/version.rb
@@ -25,5 +25,5 @@
 # https://github.com/openflighthpc/flight-service
 # ==============================================================================
 module Service
-  VERSION = '1.4.1'
+  VERSION = '1.5.0-rc1'
 end
